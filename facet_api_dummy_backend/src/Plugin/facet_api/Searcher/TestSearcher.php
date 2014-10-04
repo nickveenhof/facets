ea2d6e693d466520ad5db714d7bdcd503f1bd52d
Matthias Vandermaesen <matthias@colada.be>
2014-10-04 18:26:34 +0200

    added: a Test Searcher plugin in the dummy module (non functional)

---
+++ facet_api_dummy_backend/src/Plugin/facet_api/Searcher/TestSearcher.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Contains Drupal\facet_api_dummy_backend\Plugin\TestSearcher
+ */
+
+namespace Drupal\facet_api_dummy_backend\Plugin\FacetApi\Searcher;
+
+use Drupal\Core\Annotation\Translation;
+
+/**
+ * @FacetApiSearcher (
+ *   id = "test_searcher",
+ *   name = @Translation("Test Searcher"),
+ *   label = @Translation("Test Searcher"),
+ *   adapter = "TestAdapter",
+ *   urlProcessor = "FacetUrlProcessorStandard",
+ *   types = { "node" },
+ *   path = "",
+ *   supportFacetsMissing = TRUE,
+ *   supportFacetsMincount = TRUE,
+ *   includeDefaultFacets = FALSE
+ * )
+ */
+class TestSearcher extends SearcherPluginBase {
+  // Do nothing at the moment.
+}
