088b3aabfca571e0bc490ea1a409992e85eeea37
Mike Keran <info@mikekeran.com>
2014-10-04 10:59:20 +0200

    Dummy backend test scaffolding.

---
+++ facetapi_dummy_backend/src/Plugin/DummyBackend.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Contains Drupal\facetapi_dummy_backend\Plugin\DummyBackend.
+ */
+
+namespace Drupal\facetapi_dummy_backend\Plugin\DummyBackend;
+
+/**
+ * @FacetApiDummyBackend(
+ *   id = "facetapi_dummy_backend",
+ *   label = @Translation("FacetAPI Dummy Backend"),
+ *   description = @Translation("Dummy SearchAPI backend for testing FacetAPI.")
+ * )
+ */
+
+class DummyBackend extends BackendPluginBase {
+
+}
