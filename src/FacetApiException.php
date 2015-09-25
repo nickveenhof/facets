0aa19d50f03c8fff9c667ae36d117c4eaa8ef084
nickveenhof <nick.veenhof@gmail.com>
2015-09-25 10:50:09 +0200

    Adding facet forms

---
+++ src/FacetApiException.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\facetapi\FacetApiException.
+ */
+
+namespace Drupal\facetapi;
+
+/**
+ * Represents an exception that occurred in some part of the Facet API.
+ */
+class FacetApiException extends \Exception { }
---
+++ src/Form/FacetDeleteConfirmForm.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\facetapi\Form\FacetDeleteConfirmForm.
+ */
+
+namespace Drupal\facetapi\Form;
+
+use Drupal\Core\Entity\EntityConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Defines a confirm form for deleting a server.
+ */
+class FacetDeleteConfirmForm extends EntityConfirmFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getQuestion() {
+    return $this->t('Are you sure you want to delete the facet %name?', array('%name' => $this->entity->label()));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl() {
+    return new Url('entity.facetapi_facet.canonical', array('search_api_server' => $this->entity->id()));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->entity->delete();
+    drupal_set_message($this->t('The search server %name has been deleted.', array('%name' => $this->entity->label())));
+    $form_state->setRedirect('search_api.overview');
+  }
+
+}
