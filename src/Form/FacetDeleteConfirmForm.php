9385e7fa3b8cf080c201a92034aa124ed01232e0
Joris Vercammen <botchcake@gmail.com>
2015-10-15 12:22:46 +0200

    Issue #2592809: When deleting a facet, make sure that existing facets can't be deleted when there are still blocks that use the facet.

--- src/Form/FacetDeleteConfirmForm.php
+++ src/Form/FacetDeleteConfirmForm.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\facetapi\Form;
 
+use Drupal\block\Entity\Block;
 use Drupal\Core\Entity\EntityConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
@@ -35,6 +36,36 @@ class FacetDeleteConfirmForm extends EntityConfirmFormBase {
    */
   public function getConfirmText() {
     return $this->t('Delete');
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $is_facet_used_by_block = FALSE;
+
+    // Check if any blocks are currently using this facet.
+    $blocks = Block::loadMultiple();
+    foreach ($blocks as $block) {
+      if ($block->getPlugin() instanceof \Drupal\facetapi\Plugin\Block\FacetBlock) {
+        $facet_context_mapping = $block->getPlugin()->getConfiguration()['context_mapping']['facet'];
+        list(, $block_facet_uuid) = explode(':', $facet_context_mapping);
+
+        if ($this->entity->uuid() === $block_facet_uuid) {
+          $is_facet_used_by_block = TRUE;
+        }
+      }
+    }
+    if ($is_facet_used_by_block) {
+      $caption = '<p>' . $this->t("The facet is currently used in a block and thus can't be removed. Remove the block first.");
+      $form['#title'] = $this->getQuestion();
+      $form['description'] = array('#markup' => $caption);
+      return $form;
+    }
+
+    return parent::buildForm($form, $form_state);
   }
 
   /**
