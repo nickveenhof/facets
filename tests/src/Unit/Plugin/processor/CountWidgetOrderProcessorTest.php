2d0147bb0f602e8ea456ddd5a33032a3e54a9f26
Joris Vercammen <botchcake@gmail.com>
2015-10-21 16:47:15 +0200

    Cleanup CountWidgetOrderProcessorTest

--- tests/src/Unit/Plugin/processor/CountWidgetOrderProcessorTest.php
+++ tests/src/Unit/Plugin/processor/CountWidgetOrderProcessorTest.php
@@ -25,10 +25,23 @@ class CountWidgetOrderProcessorTest extends UnitTestCase {
   protected $processor;
 
   /**
+   * An array containing the results before the processor has ran.
+   *
+   * @var \Drupal\facetapi\Result\Result[]
+   */
+  protected $original_results;
+
+  /**
    * Creates a new processor object for use in the tests.
    */
   protected function setUp() {
     parent::setUp();
+
+    $this->original_results = [
+      new Result('llama', 10),
+      new Result('badger', 5),
+      new Result('duck', 15),
+    ];
 
     $this->processor = new CountWidgetOrderProcessor([], 'count_widget_order', []);
   }
@@ -37,13 +50,8 @@ class CountWidgetOrderProcessorTest extends UnitTestCase {
    * Test sorting ascending.
    */
   public function testAscending() {
-    $results = [
-      new Result('llama', 10),
-      new Result('badger', 5),
-      new Result('duck', 15),
-    ];
 
-    $sorted_results = $this->processor->sortResults($results, 'ASC');
+    $sorted_results = $this->processor->sortResults($this->original_results, 'ASC');
 
     $this->assertEquals(5, $sorted_results[0]->getCount());
     $this->assertEquals('badger', $sorted_results[0]->getValue());
@@ -57,14 +65,8 @@ class CountWidgetOrderProcessorTest extends UnitTestCase {
    * Test sorting descending.
    */
   public function testDescending() {
-    $results = [
-      new Result('llama', 10),
-      new Result('badger', 5),
-      new Result('duck', 15),
-    ];
 
-    /** @var Result[] $sorted_results */
-    $sorted_results = $this->processor->sortResults($results, 'DESC');
+    $sorted_results = $this->processor->sortResults($this->original_results, 'DESC');
 
     $this->assertEquals(15, $sorted_results[0]->getCount());
     $this->assertEquals('duck', $sorted_results[0]->getValue());
