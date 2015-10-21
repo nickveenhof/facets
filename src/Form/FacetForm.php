5658a97c82abe02241a50b1913693485ffcb5bc7
Joris Vercammen <botchcake@gmail.com>
2015-10-21 10:31:51 +0200

    Issue #2596359: Update to form & schema

--- src/Form/FacetForm.php
+++ src/Form/FacetForm.php
@@ -275,18 +275,22 @@ class FacetForm extends EntityForm {
       '#tree' => TRUE,
       '#open' => TRUE,
     ];
+    $form['processor_configs']['count_widget_order']['id'] = [
+      '#title' => 'id',
+      '#type' => 'textfield',
+      '#value' => 'count_widget_order'
+    ];
+    $form['processor_configs']['count_widget_order']['settings']['order'] = [
+      '#title' => 'settings-order',
+      '#type' => 'textfield',
+      '#value' => 'ASC'
+    ];
+
     foreach ($this->getProcessorPluginManager()->getDefinitions() as $id => $definition) {
       $form['processor_configs'][$id] = [
         '#type' => 'details',
         '#title' => $this->t('Processor: %id', ['%id' => $id]),
         '#open' => TRUE,
-      ];
-      $config = $this->config('facetapi.facet.' . $facet->id());
-      $config = $config->get('processor_configs')[$id];
-
-      $form['processor_configs'][$id]['id'] = [
-        '#type' => 'hidden',
-        '#value' => $id,
       ];
 
       $form['processor_configs'][$id]['settings'] = [
@@ -294,7 +298,7 @@ class FacetForm extends EntityForm {
       ];
       /** @var ProcessorInterface $build_processor */
       $build_processor = $this->getProcessorPluginManager()->createInstance($id);
-      $form['processor_configs'][$id]['settings'][] = $build_processor->buildConfigurationForm($form, $form_state, $facet);
+      $form['processor_configs'][$id]['settings'] = $build_processor->buildConfigurationForm($form, $form_state, $facet);
     }
 
     $form['status'] = [
