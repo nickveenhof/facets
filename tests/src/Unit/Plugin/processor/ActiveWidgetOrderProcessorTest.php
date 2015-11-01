63aa103b5f1d449f95710e52d8a303f2ec904198
mr.baileys <mr.baileys@383424.no-reply.drupal.org>
2015-11-01 11:05:51 +0100

    Issue #2596365 by mr.baileys: Create the "facet active" widget order processor

---
+++ tests/src/Unit/Plugin/processor/ActiveWidgetOrderProcessorTest.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\facetapi\Plugin\Processor\ActiveWidgetOrderProcessorTest.
+ */
+
+namespace Drupal\Tests\facetapi\Unit\Plugin\Processor;
+
+use Drupal\facetapi\Plugin\facetapi\processor\ActiveWidgetOrderProcessor;
+use Drupal\facetapi\Processor\WidgetOrderProcessorInterface;
+use Drupal\facetapi\Result\Result;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group facetapi
+ */
+class ActiveWidgetOrderProcessorTest extends UnitTestCase {
+
+  /**
+   * The processor to be tested.
+   *
+   * @var WidgetOrderProcessorInterface
+   */
+  protected $processor;
+
+  /**
+   * An array containing the results before the processor has ran.
+   *
+   * @var \Drupal\facetapi\Result\Result[]
+   */
+  protected $original_results;
+
+  /**
+   * Creates a new processor object for use in the tests.
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->original_results = [
+      new Result('Boxer', 'Boxer', 10),
+      new Result('Old Major', 'Old Major', 3),
+      new Result('Minimus', 'Minimus', 60),
+      new Result('Mr Whymper', 'Mr. Whymper', 1),
+      new Result('Clover', 'Clover', 50),
+    ];
+
+    $this->original_results[1]->setActiveState(true);
+    $this->original_results[2]->setActiveState(true);
+    $this->original_results[3]->setActiveState(true);
+
+    $this->processor = new ActiveWidgetOrderProcessor([], 'active_widget_order', []);
+  }
+
+  /**
+   * Test sorting ascending.
+   */
+  public function testAscending() {
+    $sorted_results = $this->processor->sortResults($this->original_results, 'ASC');
+    $expected_values = [true, true, true, false, false];
+    foreach ($expected_values as $index => $value) {
+      $this->assertEquals($value, $sorted_results[$index]->isActive());
+    }
+  }
+
+  /**
+   * Test sorting descending.
+   */
+  public function testDescending() {
+    $sorted_results = $this->processor->sortResults($this->original_results, 'DESC');
+    $expected_values = array_reverse([true, true, true, false, false]);
+    foreach ($expected_values as $index => $value) {
+      $this->assertEquals($value, $sorted_results[$index]->isActive());
+    }
+  }
+
+}
