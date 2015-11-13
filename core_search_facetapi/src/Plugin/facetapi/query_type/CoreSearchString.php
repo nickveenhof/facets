9856bb0d824c53258ea6990cbebdde69ec3bbd4c
marthinal <jm.rodriguez.velez@gmail.com>
2015-11-13 19:59:14 +0100

    For the moment set the keys when building the block(core search).

--- core_search_facetapi/src/Plugin/facetapi/query_type/CoreSearchString.php
+++ core_search_facetapi/src/Plugin/facetapi/query_type/CoreSearchString.php
@@ -30,73 +30,24 @@ class CoreSearchString extends QueryTypePluginBase {
    * {@inheritdoc}
    */
   public function execute() {
-   /* $query = $this->query;
 
-    // Alter the query here.
-    if (!empty($query)) {
-      $options = &$query->getOptions();
-
-      $field_identifier = $this->facet->getFieldIdentifier();
-      $options['search_api_facets'][$field_identifier] = array(
-        'field' => $field_identifier,
-        'limit' => 50,
-        'operator' => 'and',
-        'min_count' => 0,
-        'missing' => FALSE,
-      );
-
-      // Add the filter to the query if there are active values.
-      $active_items = $this->facet->getActiveItems();
-      if (count($active_items)) {
-        foreach ($active_items as $value) {
-          $filter = $query->createFilter();
-          $filter->condition($this->facet->getFieldIdentifier(), $value);
-          $query->filter($filter);
-        }
-      }
-    }*/
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-
-    /*
-    $build = array();
-
-    // Makes sure there was at least one match.
-    if (!$this->adapter->hasMatches) {
-      return array();
-    }
-
-    // Gets base facet query, adds facet field and filters.
-    $facet_query = clone $this->adapter->getFacetQueryExtender();
-    $query_info = $this->adapter->getQueryInfo($this->facet);
-    $facet_query->addFacetField($query_info);
-    foreach ($query_info['joins'] as $table_alias => $join_info) {
-      $facet_query->addFacetJoin($query_info, $table_alias);
-    }
-
-    // Executes query, iterates over results.
-    $result = $facet_query->execute();
-    foreach ($result as $record) {
-      $build[$record->value] = array('#count' => $record->count);
-    }
-
-    // Returns initialized build.
-    return $build;
-    */
-
-    /** @var \Drupal\facetapi\FacetManager\DefaultFacetManager $facet_manager */
+    // @TODO for the moment obtain the keys from the request here.
+    $request = \Drupal::requestStack()->getMasterRequest();
+    // @TODO avoid use \DRupal here
+    /** @var \Drupal\core_search_facetapi\FacetManager\CoreSearchFacetManager $facet_manager */
     $facet_manager = \Drupal::service('core_search_facetapi.core_manager');
-    // Sets search keys and adds active filters.
-
+    $facet_manager->setSearchKeys($request->query->get('keys'));
 
     // @TODO avoid use \DRupal here
     /** @var \Drupal\core_search_facetapi\FacetapiQuery $facet_query */
-    $facet_query = \Drupal::service('core_search_facetapi.core_manager')->getFacetQueryExtender();
-    $facet_manager->setSearchKeys($facet_query->getSearchExpression());
+    $facet_query = $facet_manager->getFacetQueryExtender();
+    //$facet_manager->setSearchKeys($facet_query->getSearchExpression());
     // @TODO hardcoded for the moment.
     $facet_query->addFacetField([
       'fields' => [
